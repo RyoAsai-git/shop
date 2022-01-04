6463fa2764f9217cc6b9f3a3c157895dc606de8e
Ryo Asai <fu.orangina0516@iCloud.com>
2022-01-04 23:04:24 +0900

    fix:中間テーブル アソシエーション修正

--- shop_api/app/models/shop.rb
+++ shop_api/app/models/shop.rb
@@ -1,3 +1,4 @@
 class Shop < ApplicationRecord
   has_many :users, through: :shop_users
+  # accepts_nested_attributes_for :shop_users
 end
--- shop_api/app/models/user.rb
+++ shop_api/app/models/user.rb
@@ -4,8 +4,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
-  has_many :brands, throught: :brand_users
-  has_many :shops, throught: :shop_users
+  has_many :brands, through: :brand_users
+  has_many :shops, through: :shop_users
+  # accepts_nested_attributes_for :brand_users
+  # accepts_nested_attributes_for :shop_users
 end
