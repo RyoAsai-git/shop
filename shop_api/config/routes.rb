887992d2e1449953601c21edfcf8379873983351
Ryo Asai <fu.orangina0516@iCloud.com>
2022-01-05 23:11:11 +0900

    shops一覧表示機能 ルーティング作成

--- shop_api/config/routes.rb
+++ shop_api/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registrations: 'auth/registrations'
   }
+
+  resources :shops, only: ['index']
 end
