38393cada21519fe7c9db3318a7656d33869cefc
Ryo Asai <fu.orangina0516@iCloud.com>
2022-01-05 23:09:59 +0900

    shops一覧表示メソッド作成

--- shop_api/app/controllers/shops_controller.rb
+++ shop_api/app/controllers/shops_controller.rb
@@ -1,2 +1,16 @@
 class ShopsController < ApplicationController
+  def index
+    shops = Shop.all
+    shops_array = shops.map do |shop|
+      {
+        id: shop.id,
+        shop_name: shop.name,
+        shop_description: shop.description,
+        shop_url: shop.shop_url,
+        created_at: shop.created_at,
+      }
+    end
+
+    render json: shops_array, status: 200
+  end
 end
