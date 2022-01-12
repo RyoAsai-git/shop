155011e949e22635c3161be2edfe440244f97b83
Ryo Asai <fu.orangina0516@iCloud.com>
2022-01-12 23:03:50 +0900

    fix:ログイン画面CSS修正

--- frontend/src/views/Welcome.vue
+++ frontend/src/views/Welcome.vue
@@ -14,9 +14,34 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
   .welcome {
     text-align: center;
     padding: 20px 0;
+  }
+  .welcome form {
+    width: 300px;
+    margin: 20px auto;
+  }
+  .welcome label {
+    display: block;
+    margin: 20px 0 10px;
+  }
+  .welcome input {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px auto;
+    border-radius: 4px;
+    border: 1px solid #eee;
+    outline: none;
+    box-sizing: border-box;
+  }
+  .welcome span{
+    font-weight: bold;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+  .welcome button {
+    margin: 20px auto;
   }
 </style>
