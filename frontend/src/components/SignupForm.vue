c20fdd23432b95aeb955f01ff190cf399e4c1e19
Ryo Asai <fu.orangina0516@iCloud.com>
2022-01-13 22:29:07 +0900

    サインアップコンポーネント作成

---
+++ frontend/src/components/SignupForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>アカウントを登録</h2>
+    <form>
+      <input type="text" required placeholder="名前" v-model="name" />
+      <input
+        type="email"
+        required
+        placeholder="メールアドレス"
+        v-model="email"
+      />
+      <input
+        type="password"
+        required
+        placeholder="パスワード"
+        v-model="password"
+      />
+      <input
+        type="password"
+        required
+        placeholder="パスワード（確認用）"
+        v-model="passwordConfirmation"
+      />
+      <button>登録する</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    };
+  },
+};
+</script>
