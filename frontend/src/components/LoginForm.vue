6542d874a354fc467b66ae19d400dfc77b771457
Ryo Asai <fu.orangina0516@iCloud.com>
2022-01-15 12:27:47 +0900

    feature:ログイン処理作成

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2>ログイン</h2>
-    <form>
+    <form @submit.prevent="login">
       <input
         type="email"
         required
@@ -14,18 +14,41 @@
         placeholder="パスワード"
         v-model="password"
       />
+      <div class="error">{{ error }}</div>
       <button>ログインする</button>
     </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
       email: "",
       password: "",
+      error: null,
     };
   },
+  methods: {
+    async login() {
+      this.error = null
+      try {
+        const res = await axios.post('http://localhost:3000/auth/sign_in', {
+          email: this.email,
+          password: this.password,
+        })
+        if (!res) {
+          throw new Error('メールアドレスかパスワードが違います')
+        }
+        console.log({ res })
+        return res
+      } catch (error) {
+        console.log({ error })
+        this.error = 'メールアドレスかパスワードが違います'
+      }
+    }
+  }
 };
 </script>
