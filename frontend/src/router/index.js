12b2f8d43bd61a08efe356d245e96caaa5685877
Ryo Asai <fu.orangina0516@iCloud.com>
2022-01-19 23:18:39 +0900

    fix:ルーティング変更

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,15 +3,27 @@ import Welcome from '../views/Welcome'
 import Home from '../views/Home'
 
 const routes = [
+  //TODO:この構成を変更する
+  // /アクセス時にhome画面が表示されるように変更？？
+  // {
+  //   path: '/',
+  //   name: 'Welcome',
+  //   component: Welcome
+  // },
+  // {
+  //   path: '/home',
+  //   name: 'Home',
+  //   component: Home
+  // }
   {
     path: '/',
-    name: 'Welcome',
-    component: Welcome
+    name: 'Home',
+    component: Home,
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: Home
+    path: '/welcome',
+    name: 'Welcome',
+    component: Welcome
   }
 ]
 
