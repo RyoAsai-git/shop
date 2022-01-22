62962bd8e527a2dc61b923d7f794bfd279613f81
Ryo Asai <fu.orangina0516@iCloud.com>
2022-01-22 20:48:43 +0900

    feature:home画面ヘッダー作成

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,12 +1,48 @@
 <template>
-<!-- ここにサインアップページとログインページのルーティング作成 -->
-  <div>home画面です</div>
+  <!-- ここにサインアップページとログインページのルーティング作成 -->
+  <body>
+    <header>
+      <h1 class="headline">
+        <a>サンプルのサイト</a>
+      </h1>
+      <ul class="nav-list">
+        <li class="nav-list-item">
+          <a>Home</a>
+        </li>
+        <li class="nav-list-item">About</li>
+        <li class="nav-list-item">Topic</li>
+      </ul>
+    </header>
+    <div>
+    </div>
+  </body>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
 
 <style scoped>
+* {
+  margin:0; padding:0;
+}
+header {
+  width: 100%;
+  padding: 15px 0;
+  margin: 0 auto;
+  text-align: center;
+}
+header .headline{
+  font-size: 32px;
+}
+.nav-list {
+  text-align: center;
+  padding: 10px 0;
+  margin: 0 auto;
+}
+.nav-list-item {
+  list-style: none;
+  display: inline-block;
+  margin: 0 20px;
+}
 </style>
