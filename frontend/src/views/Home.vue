0139f8aea4498065bae0107c2eb9d79331ca0a05
Ryo Asai <fu.orangina0516@iCloud.com>
2022-01-23 23:02:06 +0900

    fix:不要な記述削除

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -5,7 +5,7 @@
         <a>サンプルのサイト</a>
       </h1>
       <ul class="nav-list">
-        <li class="nav-list-item">Ï
+        <li class="nav-list-item">
           <a>Home</a>
         </li>
         <li class="nav-list-item">
