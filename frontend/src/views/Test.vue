857c1bebc431e33b97b6076814d38b66b3bbe4ca
Ryo Asai <fu.orangina0516@iCloud.com>
2022-01-25 23:00:47 +0900

    fix:モーダルのcss修正

--- frontend/src/views/Test.vue
+++ frontend/src/views/Test.vue
@@ -1,22 +1,24 @@
 <template>
   <body>
-    <div id="app">
-      <button v-on:click="openModal">Click</button>
-    </div>
-    <div id="overlay" v-show="showContent">
-      <div id="content">
-        <LoginForm v-if="!shouldShowLoginForm" />
-        <p class="change-form">
-          初めての方は<span @click="shouldShowLoginForm = false">こちら</span
-          >をクリック
-        </p>
-        <SignupForm v-if="shouldShowLoginForm" />
-        <p class="change-form">
-          アカウントをお持ちの方は<span @click="shouldShowLoginForm = true"
-            >こちら</span
-          >をクリック
-        </p>
-        <button v-on:click="closeModal">close</button>
+    <div class="main">
+      <div id="app">
+        <button v-on:click="openModal">Click</button>
+      </div>
+      <div id="overlay" v-show="showContent">
+        <div id="content" class="modal">
+          <LoginForm v-if="!shouldShowLoginForm" />
+          <p class="change-form">
+            初めての方は<span @click="shouldShowLoginForm = false">こちら</span
+            >をクリック
+          </p>
+          <SignupForm v-if="shouldShowLoginForm" />
+          <p class="change-form">
+            アカウントをお持ちの方は<span @click="shouldShowLoginForm = true"
+              >こちら</span
+            >をクリック
+          </p>
+          <button v-on:click="closeModal">close</button>
+        </div>
       </div>
     </div>
   </body>
@@ -50,6 +52,10 @@ export default {
 </script>
 
 <style>
+.main {
+  height: 100%;
+  widows: 100%;
+}
 #overlay {
   z-index: 1;
   position: fixed;
@@ -62,16 +68,46 @@ export default {
   align-items: center;
   justify-content: center;
 }
-
 #content {
   z-index: 2;
   width: 50%;
   padding: 1em;
   background: #fff;
 }
-
-/* .welcome {
+.modal {
+  background: #fff;
+  height: 50%;
+  width: 50%;
   text-align: center;
   padding: 20px 0;
-} */
+}
+.modal form {
+  width: 300px;
+  margin: 20px auto;
+}
+.modal label {
+  display: block;
+  margin: 20px 0 10px;
+}
+.modal input {
+  width: 100%;
+  padding: 12 px 20 px;
+  margin: 8 px auto;
+  border-radius: 4 px;
+  border: 1 px solid #eee;
+  outline: none;
+  box-sizing: border-box;
+}
+.modal span {
+  font-weight: bold;
+  text-decoration: underline;
+  cursor: pointer;
+}
+.modal button {
+  margin: 20px auto;
+}
+.change-form {
+  font-size: 14px;
+  margin: 10px;
+}
 </style>
