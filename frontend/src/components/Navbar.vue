e19493a7a8cee44213bb1c193377628c8cd0db5c
Ryo Asai <fu.orangina0516@iCloud.com>
2022-01-30 22:27:05 +0900

    feature:navbar用のコンポーネント土台作成

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div class="navbar">
+      <ul>
+        <li>test1</li>
+        <li>test2</li>
+        <li>test3</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
