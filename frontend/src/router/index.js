bc79ea0a40642a614da245c7387841fd5a3cec3d
Ryo Asai <fu.orangina0516@iCloud.com>
2022-01-30 22:19:40 +0900

    fix:home画面のルーティング作成

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from '../views/Welcome'
+import Home from '../views/Home'
 
 const routes = [
   {
@@ -7,6 +8,11 @@ const routes = [
     name: 'Welcome',
     component: Welcome,
   },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home,
+  }
 ]
 
 const router = createRouter({
