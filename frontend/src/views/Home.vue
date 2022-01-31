52d73fb4b58f800cab60879ad3cb4396934f3e82
Ryo Asai <fu.orangina0516@iCloud.com>
2022-01-31 21:51:30 +0900

    nabvar仮作成

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,9 +2,10 @@
   <div class="main">
     <div class="navbar">
       <ul class="navbar-list">
-        <li>test1</li>
-        <li>test2</li>
-        <li>test3</li>
+        <li>探す</li>
+        <li>お気に入り</li>
+        <li>マイページ</li>
+        <li>設定</li>
       </ul>
     </div>
   </div>
@@ -43,17 +44,27 @@ body {
 }
 
 .navbar-list {
-
 }
 
 li:first-child {
-  padding-top: 0px;
+  padding-top: 20px;
 }
 
 li {
+  text-align: center;
+  line-height: 20px;
   padding-top: 20px;
-  list-style-type: none
+  padding-bottom: 20px;
+  padding-left: 40px;
+  list-style-type: none;
+  transition: background-color 0.5s;
+  font-size: 17px;
 }
 
+li:hover {
+  background-color: rgb(191, 207, 207);
+  transition: background-color 100ms;
+}
 
+/* color: #0096a9 */
 </style>
