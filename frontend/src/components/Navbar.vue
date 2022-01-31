56a119e0ede8c92104c89fa5aba730967e9b54a3
Ryo Asai <fu.orangina0516@iCloud.com>
2022-01-31 22:03:05 +0900

    fix:navbarのコンポーネント化

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,23 +1,47 @@
 <template>
-  <div class="test">
-    <div class="navbar">
-      <ul>
-        <li>test1</li>
-        <li>test2</li>
-        <li>test3</li>
-      </ul>
-    </div>
+  <div class="navbar">
+    <ul class="navbar-list">
+      <li>探す</li>
+      <li>お気に入り</li>
+      <li>マイページ</li>
+      <li>設定</li>
+    </ul>
   </div>
 </template>
 
 <script>
-
 </script>
 
 <style scoped>
-  .navbar {
-    background: black;
-    height: 100%;
-    width: 20%;
-  }
+.navbar {
+  height: 100%;
+  width: 20%;
+  border-right: 2px solid #f1f1f1;
+}
+
+.navbar-list {
+}
+
+li:first-child {
+  padding-top: 20px;
+}
+
+li {
+  text-align: center;
+  line-height: 20px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  padding-left: 40px;
+  list-style-type: none;
+  transition: background-color 0.5s;
+  font-size: 17px;
+}
+
+li:hover {
+  background-color: rgb(191, 207, 207);
+  transition: background-color 100ms;
+}
+
+/* ボタン選択時の色候補 */
+/* color: #0096a9 */
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,22 +1,15 @@
 <template>
   <div class="main">
-    <div class="navbar">
-      <ul class="navbar-list">
-        <li>探す</li>
-        <li>お気に入り</li>
-        <li>マイページ</li>
-        <li>設定</li>
-      </ul>
-    </div>
+    <Navbar />
   </div>
 </template>
 
 <script>
-// import Navbar from "../components/Navbar.vue";
+import Navbar from "../components/Navbar.vue";
 
 export default {
   components: {
-    // Navbar,
+    Navbar,
   },
 };
 </script>
@@ -37,34 +30,5 @@ body {
   width: 100%;
 }
 
-.navbar {
-  height: 100%;
-  width: 20%;
-  border-right: 2px solid #f1f1f1;
-}
 
-.navbar-list {
-}
-
-li:first-child {
-  padding-top: 20px;
-}
-
-li {
-  text-align: center;
-  line-height: 20px;
-  padding-top: 20px;
-  padding-bottom: 20px;
-  padding-left: 40px;
-  list-style-type: none;
-  transition: background-color 0.5s;
-  font-size: 17px;
-}
-
-li:hover {
-  background-color: rgb(191, 207, 207);
-  transition: background-color 100ms;
-}
-
-/* color: #0096a9 */
 </style>
