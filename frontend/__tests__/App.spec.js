91b018e886f95a4a916351a94f60867a48a07b26
Ryo Asai <fu.orangina0516@iCloud.com>
2022-02-11 18:58:26 +0900

    プロジェクトへjest追加

--- frontend/__tests__/App.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Component from '@/App.vue'
-
-describe('Testing App component', () => {
-    it('is a Vue instance', () => {
-        const wrapper = mount(Component)
-        expect(wrapper.isVueInstance).toBeTruthy()
-    })
-})
