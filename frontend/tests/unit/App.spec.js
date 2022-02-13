deae292a8f012f64bb1162dfecddea518ae76b3a
Ryo Asai <fu.orangina0516@iCloud.com>
2022-02-13 15:51:30 +0900

    feature:LoginFormのテキストが表示されているかのテストコード作成

--- frontend/tests/unit/App.spec.js
+++ frontend/tests/unit/App.spec.js
@@ -1,11 +1,11 @@
 import {
     mount
-} from '@vue/test-utils';
-import Component from '@/App.vue';
+} from '@vue/test-utils'
+import Component from '@/components/LoginForm.vue'
 
-describe('Testing App component', () => {
-    it('is a Vue instance', () => {
+describe('LoginForm', () => {
+    test('display LoginForm Text', () => {
         const wrapper = mount(Component)
-        expect(wrapper.isVueInstance).toBeTruthy()
+        expect(wrapper.text()).toBe("ログインログインする")
     })
 })
