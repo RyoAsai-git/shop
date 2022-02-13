f33c28550280c68736271258c8df9c058774ef63
Ryo Asai <fu.orangina0516@iCloud.com>
2022-02-13 17:55:35 +0900

    fix:テスト用コンポーネント名変更

--- frontend/tests/unit/LoginForm.spec.js
+++ frontend/tests/unit/LoginForm.spec.js
@@ -1,11 +1,11 @@
 import {
     mount
 } from '@vue/test-utils'
-import Component from '@/components/LoginForm.vue'
+import LoginForm from '@/components/LoginForm.vue'
 
 describe('LoginForm', () => {
     test('display LoginForm Text', () => {
-        const wrapper = mount(Component)
+        const wrapper = mount(LoginForm)
         expect(wrapper.text()).toBe("ログインログインする")
     })
 })
