c1d1bf63571e27ec8384ee21d5ed089e4dad947c
Ryo Asai <fu.orangina0516@iCloud.com>
2022-02-13 12:49:41 +0900

    fix:テストファイル修正

--- frontend/tests/unit/App.spec.js
+++ frontend/tests/unit/App.spec.js
@@ -1,7 +1,7 @@
 import {
     mount
 } from '@vue/test-utils';
-import Component from '@App.vue';
+import Component from '@/App.vue';
 
 describe('Testing App component', () => {
     it('is a Vue instance', () => {
