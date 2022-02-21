82588566e83f7544e815b487e49b8cd64841c0d7
Ryo Asai <fu.orangina0516@iCloud.com>
2022-02-22 00:03:57 +0900

    fix:brandArea 画像大きさ変更

--- frontend/src/components/BrandArea.vue
+++ frontend/src/components/BrandArea.vue
@@ -1,21 +1,32 @@
 <template>
   <div class="brand_area">
-    <div class="">
-
-    </div>
+    <img
+      src="https://cld.fashionsnap.com/image/upload/asset/brand/images/2021/05/auraleelogo20210513-000.jpg"
+      alt=""
+      v-on:click="openModal"
+      class="brand_image"
+    />
   </div>
 </template>
 
 <script>
-
 </script>
 
 <style scoped>
-.brand_area{
+.brand_area {
   height: 100%;
   width: 80%;
   position: absolute;
   left: 20%;
   top: 0;
 }
+
+.brand_image {
+  background-color: #ccc;
+  width: 30%;
+  -webkit-border-radius: 20px;
+  -moz-border-radius: 20px;
+  border:1px solid #d3d3d3;
+}
+
 </style>
