c2d691e1a69f1eaa6ac2f31297c23e90d663e537
Ryo Asai <fu.orangina0516@iCloud.com>
2022-02-28 23:47:27 +0900

    fix:main.jsのインテンド修正

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,28 +3,21 @@ import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 
-   import {
-       library
-   } from "@fortawesome/fontawesome-svg-core";
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-   // 無料で使えるフォントをインポート
-   import {
-       fab
-   } from "@fortawesome/free-brands-svg-icons";
-   import {
-       far
-   } from "@fortawesome/free-regular-svg-icons";
-   import {
-       fas
-   } from "@fortawesome/free-solid-svg-icons";
-   // コンポネントをインポート
-   import {
-       FontAwesomeIcon,
-       FontAwesomeLayers,
-       FontAwesomeLayersText
-   } from "@fortawesome/vue-fontawesome";
-   // ライブラリに追加
-   library.add(fas, far, fab);
+// 無料で使えるフォントをインポート
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+// コンポネントをインポート
+import {
+    FontAwesomeIcon,
+    FontAwesomeLayers,
+    FontAwesomeLayersText
+} from "@fortawesome/vue-fontawesome";
+
+// ライブラリに追加
+library.add(fas, far, fab);
 
 createApp(App)
 .use(router)
