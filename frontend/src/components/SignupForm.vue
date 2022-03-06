bc4e713cff2b3792f542ff45f0d40505f0ae760e
Ryo Asai <fu.orangina0516@iCloud.com>
2022-03-06 21:59:16 +0900

    fix:サインアップ、ログインコンポーネントのインテンド修正

--- frontend/src/components/SignupForm.vue
+++ frontend/src/components/SignupForm.vue
@@ -70,13 +70,13 @@ export default {
 
 <style scoped>
 button {
-    text-decoration: none;
-    background: #51b392;
-    color: white;
-    font-weight: bold;
-    border: 0;
-    border-radius: 3px;
-    padding: 10px 20px;
-    cursor: pointer;
+  text-decoration: none;
+  background: #51b392;
+  color: white;
+  font-weight: bold;
+  border: 0;
+  border-radius: 3px;
+  padding: 10px 20px;
+  cursor: pointer;
 }
 </style>
