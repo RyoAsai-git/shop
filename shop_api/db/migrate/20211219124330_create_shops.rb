cb77904afa62196f6f234f7e7b6b7e7e8a910c60
Ryo Asai <fu.orangina0516@iCloud.com>
2022-03-07 22:04:19 +0900

    fix:shopテーブル ブランド、画像カラム追加

--- shop_api/db/migrate/20211219124330_create_shops.rb
+++ shop_api/db/migrate/20211219124330_create_shops.rb
@@ -2,6 +2,8 @@ class CreateShops < ActiveRecord::Migration[6.0]
   def change
     create_table :shops do |t|
       t.string :name, limit: 100, null: false
+      t.string :image
+      t.references :brand foreign_key: true
       t.text :description
       t.text :shop_url
       t.timestamps
