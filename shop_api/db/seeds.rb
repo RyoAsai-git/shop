55ca996fdec5168a2f5078c86c2f22dc2bf567d1
Ryo Asai <fu.orangina0516@iCloud.com>
2022-03-08 23:53:47 +0900

    fix:shop seedファイル修正

--- shop_api/db/seeds.rb
+++ shop_api/db/seeds.rb
@@ -1,8 +1,9 @@
 3.times do |number|
   Shop.create(
     name: "#{number}番目のお店",
+    image: "https://cld.fashionsnap.com/image/upload/asset/brand/images/2021/05/auraleelogo20210513-000.jpg",
     description: "#{number}番目の店舗説明",
-    shop_url: "http://test#{number}/shop",
+    shop_url: "https://auralee.jp/",
     # puts "#{number}番目のお店を作成しました"
   )
   puts "#{number}番目のメッセージを作成しました"
