ac5f03318dab0490c2910de0fc7cb7c58c9e96f3
Ryo Asai <fu.orangina0516@iCloud.com>
2022-03-10 00:23:52 +0900

    fix:中間テーブルのモデル修正

--- shop_api/app/models/brand_user.rb
+++ shop_api/app/models/brand_user.rb
@@ -1,2 +1,4 @@
 class BrandUser < ApplicationRecord
+  belongs_to :users, optional: ture
+  belongs_to :brands, optional: true
 end
--- shop_api/app/models/shop_brand.rb
+++ shop_api/app/models/shop_brand.rb
@@ -1,4 +1,4 @@
 class ShopBrand < ApplicationRecord
-  belongs_to :shops
-  belongs_to :brands
+  belongs_to :shops, optional: ture
+  belongs_to :brands, optional: true
 end
