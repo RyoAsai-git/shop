6a822d7446bc35906aff00e023d2193a445557e9
Ryo Asai <fu.orangina0516@iCloud.com>
2022-03-10 00:16:56 +0900

    fix:マイグレーションファイル インテンド修正

--- shop_api/db/migrate/20220104134512_create_shop_users.rb
+++ shop_api/db/migrate/20220104134512_create_shop_users.rb
@@ -1,7 +1,7 @@
 class CreateShopUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :shop_users do |t|
-      t.references :user,  index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
       t.references :shop, index: true, foreign_key: true
       t.timestamps
     end
--- shop_api/db/migrate/20220307130727_create_shop_brands.rb
+++ shop_api/db/migrate/20220307130727_create_shop_brands.rb
@@ -1,7 +1,7 @@
 class CreateShopBrands < ActiveRecord::Migration[6.0]
   def change
     create_table :shop_brands do |t|
-      t.references :shop, index: true, foreign_key: true
+      t.references :shop,  index: true, foreign_key: true
       t.references :brand, index: true, foreign_key: true
       t.timestamps
     end
