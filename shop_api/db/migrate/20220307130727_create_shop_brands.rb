cf02566e5d72c97b89ece03c3d66bf4012bc8c70
Ryo Asai <fu.orangina0516@iCloud.com>
2022-03-10 00:16:32 +0900

    fix:マイグレーションファイル修正

--- shop_api/db/migrate/20220307130727_create_shop_brands.rb
+++ shop_api/db/migrate/20220307130727_create_shop_brands.rb
@@ -1,8 +1,8 @@
 class CreateShopBrands < ActiveRecord::Migration[6.0]
   def change
     create_table :shop_brands do |t|
-      t.references :shop_id, index: true, foreign_key: true
-      t.references :brand_id, index: true, foreign_key: true
+      t.references :shop, index: true, foreign_key: true
+      t.references :brand, index: true, foreign_key: true
       t.timestamps
     end
   end
