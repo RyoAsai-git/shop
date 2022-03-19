9dfd47ae73cb0e3af299466286ca3009346f318e
Ryo Asai <fu.orangina0516@iCloud.com>
2022-03-20 00:00:20 +0900

    fix:brandsコントローラーのindexメソッド修正

--- shop_api/app/controllers/brands_controller.rb
+++ shop_api/app/controllers/brands_controller.rb
@@ -5,6 +5,12 @@ class BrandsController < ApplicationController
       {
         id: brand.id,
         name: brand.name,
+        image: brand.image,
+        description: brand.description,
+        brand_url: brand.brand_url
       }
+    end
+
+    render json: brands_array, status: 200
   end
 end
