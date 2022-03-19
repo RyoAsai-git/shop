b1f643720e73291f6b3af469a64ad0e061640933
Ryo Asai <fu.orangina0516@iCloud.com>
2022-03-19 23:38:15 +0900

    indexアクション作成

--- shop_api/app/controllers/brands_controller.rb
+++ shop_api/app/controllers/brands_controller.rb
@@ -1,2 +1,10 @@
 class BrandsController < ApplicationController
+  def index
+    brands = Brand.all
+    brands_array = brands.map do |brand|
+      {
+        id: brand.id,
+        name: brand.name,
+      }
+  end
 end
