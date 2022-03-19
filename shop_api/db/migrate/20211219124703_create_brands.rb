6583835b5943f0584e702d37277e6f35226c0329
Ryo Asai <fu.orangina0516@iCloud.com>
2022-03-19 23:47:39 +0900

    fix:brandテーブルのカラム追加 ダミーデータ作成箇所修正

--- shop_api/db/migrate/20211219124703_create_brands.rb
+++ shop_api/db/migrate/20211219124703_create_brands.rb
@@ -2,6 +2,9 @@ class CreateBrands < ActiveRecord::Migration[6.0]
   def change
     create_table :brands do |t|
       t.string :name, limit: 100, null: false
+      t.string :image
+      t.text :description
+      t.text :brand_url
       t.references :shops, index: true, foreign_key: true
       t.timestamps
     end
