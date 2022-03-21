2afdc63c0c35135bc00b9dc9c80eb3c4fd6d2ffc
Ryo Asai <fu.orangina0516@iCloud.com>
2022-03-22 00:06:52 +0900

    途中経過

--- shop_api/app/controllers/shops_controller.rb
+++ shop_api/app/controllers/shops_controller.rb
@@ -10,11 +10,28 @@ class ShopsController < ApplicationController
         description: shop.description,
         url: shop.shop_url,
         created_at: shop.created_at,
-        brand: shop.shop_brands
+        # brand: shop.shop_brands
+        brand_array: shop.shop_brands.map do |brand|
+          {
+            id: brand.brand.id,
+            # shop_id: brand.shop_id,
+            # brand_id: brand.brand_id,
+            # created_at: brand.created_at,
+            # updated_at: brand.updated_at,
+            # name: brand.name,
+            # image: brand.image,
+            # description: brand.description,
+            # url: brand.brand_url,
+            name: brand.brand.name,
+            image: brand.brand.image,
+            description: brand.brand.description,
+            url: brand.brand.brand_url,
+          }
+        end
       }
     end
 
     # render json: shops_array, status: 200
-    render json: shops_array.to_json(include: :brand)
+    render json: shops_array.to_json(include: :brand), status: 200
   end
 end
