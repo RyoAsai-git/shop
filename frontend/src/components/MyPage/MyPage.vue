228d24a865b429249bfd7d11ff9ab26193e991ff
Ryo Asai <fu.orangina0516@iCloud.com>
2022-03-26 22:50:19 +0900

    feature:マイページ土台作成

--- frontend/src/components/MyPage/MyPage.vue
+++ frontend/src/components/MyPage/MyPage.vue
@@ -1,18 +1,29 @@
 <template>
-<div>
-  <div class="content">
-    <div class=""></div>
+  <div>
+    <div class="content">
+      <div class="my_page">
+        <div class="my_page-profile">
+          <img
+            src="https://1.bp.blogspot.com/-DU9jll2ZQ38/XexqGlVzO9I/AAAAAAABWdQ/m0lQONbEfSgEjIN14h7iIfRh8WS5qwrFACNcBGAsYHQ/s1600/gal_o_man.png"
+            alt=""
+            class="profile-image"
+          />
+        </div>
+      </div>
+      <div class="favorite-shop">
+        <img src="" alt="" class="favorite-shop-image" />
+      </div>
+      <div class="favorite-brand">
+        <img src="" alt="" class="favorite-brand-image" />
+      </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 
 <style scoped>
-
 </style>
