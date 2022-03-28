298f9f5bf24be2e9e4399c474ec6205a0a928d42
Ryo Asai <fu.orangina0516@iCloud.com>
2022-03-28 23:53:07 +0900

    fix:マイページコントローラー修正

--- shop_api/app/controllers/mypages_controller.rb
+++ shop_api/app/controllers/mypages_controller.rb
@@ -1,7 +1,29 @@
 class MypagesController < ApplicationController
   def show
-    user = User.find(params[:id])
-    # user = User.include(:brand_user).where(params[:id])
+    user = User.includes(brand_users: :brand).find(params[:id])
+    
     render json: user, status: 200;
+  end
+
+  def index
+    users = User.all.includes(brand_users: :brand)
+    users_array = users.map do |user|
+      {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        brands: user.brand_users.map do |brand|
+          {
+            id: brand.brand.id,
+            name: brand.brand.name,
+            image: brand.brand.image,
+            description: brand.brand.description,
+            url: brand.brand.brand_url,
+          }
+        end
+      }
+    end
+
+    render json: users_array.to_json(include: :brand), status: 200
   end
 end
