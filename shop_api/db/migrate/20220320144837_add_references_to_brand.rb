d80db0f900bd0c21d8fb7b9ed29df6965bc8d193
Ryo Asai <fu.orangina0516@iCloud.com>
2022-03-29 00:04:43 +0900

    fix:リファレンス追加

--- shop_api/db/migrate/20220320144837_add_references_to_brand.rb
+++ shop_api/db/migrate/20220320144837_add_references_to_brand.rb
@@ -1,5 +1,6 @@
 class AddReferencesToBrand < ActiveRecord::Migration[6.0]
   def change
     add_reference :brands, :shop, foreign_key: true
+    add_reference :brands, :user, foreign_key: true
   end
 end
