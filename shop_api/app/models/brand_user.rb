32941e6802469429f3a60b7985505a2702349146
Ryo Asai <fu.orangina0516@iCloud.com>
2022-04-04 00:33:09 +0900

    fix:ユーザーとブランドの組み合わせが一意になるよう修正 バリデーション追記

--- shop_api/app/models/brand_user.rb
+++ shop_api/app/models/brand_user.rb
@@ -1,4 +1,6 @@
 class BrandUser < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :brand, optional: true
+
+  validates :user_id, uniqueness: { scope: :brand_id }
 end
--- shop_api/app/models/user.rb
+++ shop_api/app/models/user.rb
@@ -4,13 +4,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
   has_many :brand_users
   has_many :brands, through: :brand_users
 
   has_many :shop_users
   has_many :shops, through: :shop_users
-  # accepts_nested_attributes_for :brand_users
-  # accepts_nested_attributes_for :shop_users
 end
