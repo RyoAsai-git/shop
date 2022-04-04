4af78ce6ccbd05821b7c9f2c92449e1810e3bae6
Ryo Asai <fu.orangina0516@iCloud.com>
2022-04-05 00:51:30 +0900

    fix:ルーティング設定後のrouter-link表示後の文字色変更処理追加

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="navbar">
+    <ul class="navbar-list">
+      <li>
+        <router-link
+          to="/search"
+          exact
+          class="item-text"
+          >ショップ</router-link
+        >
+      </li>
+      <li>
+        <router-link
+          to="/brand"
+          exact
+          class="item-text"
+          >ブランド</router-link
+        >
+      </li>
+      <li>
+        <router-link
+          to="/myPage"
+          exact
+          class="item-text"
+          >マイページ</router-link
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      search: false,
+      brand: false,
+      myPage: false
+    }
+  },
+};
+</script>
+
+<style scoped>
+
+ul li a {
+  text-decoration: none;
+}
+
+.navbar {
+  height: 100%;
+  width: 15%;
+  border-right: 2px solid #f1f1f1;
+  position: fixed;
+  left: 0;
+  top: 0;
+}
+
+li:first-child {
+  padding-top: 20px;
+}
+
+li {
+  text-align: center;
+  line-height: 20px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  transition: background-color 0.5s;
+  font-size: var(--main-font-size);
+  cursor: pointer;
+}
+
+li:hover {
+  background-color: rgb(191, 207, 207);
+  transition: background-color 100ms;
+}
+
+.router-link-active {
+  color: var(--main-bg-color);
+}
+</style>
