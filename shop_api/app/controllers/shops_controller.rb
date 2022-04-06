9bd9815d8bda0b14672a3fc1b41c398fed55eba6
Ryo Asai <fu.orangina0516@iCloud.com>
2022-04-07 00:55:02 +0900

    feature:お気に入り店舗 作成機能 削除機能API実装

--- shop_api/app/controllers/shops_controller.rb
+++ shop_api/app/controllers/shops_controller.rb
@@ -23,4 +23,28 @@ class ShopsController < ApplicationController
 
     render json: shops_array.to_json(include: :brand), status: 200
   end
+
+  def likes
+    shop = Shop.find(params[:shop_id])
+    user = User.find(params[:user_id])
+
+    like = ShopUser.new(user_id: user.id, shop_id: shop.id)
+    if like.save
+    render json: { user_id: user.id, shop_id: shop.id, message: '成功しました' }, status: 200
+    else
+      render json: { message: '保存できませんでした', errors: like.errors.messages }, status: 400
+    end
+  end
+
+  def delete_likes
+    shop = Shop.find(params[:shop_id])
+    user = User.find(params[:user_id])
+
+    like = ShopUser.find_by(shop_id: shop.id, user_id: user.id)
+    if like.destroy
+      render json: { user_id: user.id, shop_id: shop.id, message: '削除に成功しました' }, status: 200
+    else
+      render json: { message: '削除できませんでした', errors: like.errors.messages }, status: 400
+    end
+  end
 end
