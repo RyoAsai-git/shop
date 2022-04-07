a65a82f8f6f04a9fcc7099bd42df38527f7afc29
Ryo Asai <fu.orangina0516@iCloud.com>
2022-04-07 19:35:52 +0900

    fix:古いfont-awesome読み込みよう記述削除

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,35 +3,15 @@ import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 
-// import { library } from "@fortawesome/fontawesome-svg-core";
-
-// // 無料で使えるフォントをインポート
-// import { fab } from "@fortawesome/free-brands-svg-icons";
-// import { far } from "@fortawesome/free-regular-svg-icons";
-// import { fas } from "@fortawesome/free-solid-svg-icons";
-// // コンポネントをインポート
-// import {
-//     FontAwesomeIcon,
-//     FontAwesomeLayers,
-//     FontAwesomeLayersText
-// } from "@fortawesome/vue-fontawesome";
-
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 
-// // ライブラリに追加
-// library.add(fas, far, fab);
-
 library.add(fas, far, fab)
-// Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 createApp(App)
 .use(router)
 .component('font-awesome-icon', FontAwesomeIcon)
-// .component("FAIcon", FontAwesomeIcon)
-// .component('FALayers', FontAwesomeLayers)
-// .component('FAText', FontAwesomeLayersText)
 .mount('#app')
