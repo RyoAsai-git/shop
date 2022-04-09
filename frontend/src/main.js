1501d9c4f677c1d2ce14b6ddd9c5ddbc2413f2e2
Ryo Asai <fu.orangina0516@iCloud.com>
2022-04-09 13:27:23 +0900

    fix:font-awesome呼び出し箇所修正 main.js修正

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,8 +10,15 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 
 library.add(fas, far, fab)
+// createApp(App).component('font-awesome-icon', FontAwesomeIcon)
 
-createApp(App)
-.use(router)
-.component('font-awesome-icon', FontAwesomeIcon)
-.mount('#app')
+// createApp(App)
+// // .component('font-awesome-icon', FontAwesomeIcon)
+// .component('fa', FontAwesomeIcon )
+// .use(router)
+// .mount('#app')
+
+var app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon )
+app.use(router)
+app.mount('#app')
