8c5aff54a1c8c27694e705d451908a7ea19d60e7
Ryo Asai <fu.orangina0516@iCloud.com>
2022-04-09 19:35:07 +0900

    fix:検索バーにアイコン追加 ショップ一覧のレイアウト微調整

--- frontend/src/components/Search/Search.vue
+++ frontend/src/components/Search/Search.vue
@@ -2,6 +2,7 @@
   <main>
     <div class="search-area">
       <input type="text" class="search-bar" v-model="keyword" />
+      <font-awesome-icon :icon="['fas', 'search']" class="icon" />
     </div>
     <div class="content">
       <div class="shop-area">
@@ -87,6 +88,12 @@ export default {
   outline-color: var(--main-bg-color);
 }
 
+.icon {
+  margin-left: 10px;
+  font-size: 19px;
+  color: var(--main-bg-color);
+}
+
 .shop-area {
   margin-top: 40px;
   overflow: auto;
@@ -96,14 +103,14 @@ export default {
   display: inline-block;
   height: 100px;
   width: 300px;
-  padding-left: 50px;
+  padding-left: 75px;
   margin: 20px auto 0;
   transition-duration: 0.5s;
   cursor: pointer;
 }
 
 .shop-area-image {
-  width: 100%;
+  width: 107%;
   border-radius: 20px;
   background-color: #ccc;
   aspect-ratio: 5 / 3;
