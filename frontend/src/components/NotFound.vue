2d41162741cd4e66f490a7ec112d4e0768de0817
Ryo Asai <fu.orangina0516@iCloud.com>
2022-04-12 17:15:48 +0900

    fix:404ページにhome画面へのリダイレクトボタン作成

--- frontend/src/components/NotFound.vue
+++ frontend/src/components/NotFound.vue
@@ -3,23 +3,23 @@
     <h1>404 page not found</h1>
     <h2>ページが見つかりません。</h2>
     <h3 class="error-description">
-      お探しのページが見つかりませんでした。<br>
-      URLが間違っているか、ページが存在しません。<br>
+      お探しのページが見つかりませんでした。<br />
+      URLが間違っているか、ページが存在しません。<br />
     </h3>
-    <div class="home-button">
-      <button @click="redirectToHome">HOMEに戻る</button>
+    <div>
+      <button class="home-button" @click="redirectToHome">HOMEに戻る</button>
     </div>
   </div>
 </template>
 
 <script>
-export default ({
+export default {
   methods: {
     redirectToHome() {
       this.$router.push({ name: "Shop" });
-    }
-  }
-})
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -32,4 +32,14 @@ export default ({
   margin-top: 30px;
 }
 
+.home-button {
+  margin-top: 40px;
+  color: #fff;
+  width: 150px;
+  border-radius: 20px;
+  border: solid #d3d3d3;
+  background-color: var(--main-bg-color);
+  line-height: 40px;
+  cursor: pointer;
+}
 </style>
