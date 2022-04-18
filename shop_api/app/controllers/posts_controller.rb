e354ff515dd63e5f6618505c03e740e60bf031dd
Ryo Asai <fu.orangina0516@iCloud.com>
2022-04-18 16:54:31 +0900

    fix:不要なコメントアウト削除

--- shop_api/app/controllers/posts_controller.rb
+++ shop_api/app/controllers/posts_controller.rb
@@ -1,50 +1,12 @@
-# class PostsController < ApplicationController
-#   def index
-#     render json: Post.all, methods: [:image_url]  # ここを変更
-#   end
-
-#   def create
-#     post = Post.new(post_params)
-#     if post.save
-#       render json: post, methods: [:image_url]  # ここを変更
-#     else
-#       render json: post.errors, status: 422
-#     end
-#   end
-
-#   def destroy
-#     post = Post.find(params[:id])
-#     post.destroy!
-#     render json: post
-#   end
-
-#   private
-
-#   def post_params
-#     params.permit(:title, :image)
-#   end
-# end
-
 class PostsController < ApplicationController
   def index
-    # post = Post.all
-    # render json: post, status: 200
     render json: Post.all, methods: [:image_url]
   end
 
-  # def create
-  #   post = Post.new(post_params)
-  #   # post = image_url(post)
-  #   if post.save
-  #     render json: post
-  #   else
-  #     render json: post.errors, status: 422
-  #   end
-  # end
   def create
     post = Post.new(post_params)
     if post.save
-      render json: post, methods: [:image_url]  # ここを変更
+      render json: post, methods: [:image_url]
     else
       render json: post.errors, status: 422
     end
--- shop_api/app/models/post.rb
+++ shop_api/app/models/post.rb
@@ -4,7 +4,6 @@ class Post < ApplicationRecord
   has_one_attached :image
 
   def image_url
-    # 紐づいている画像のURLを取得する
     image.attached? ? url_for(image) : nil
   end
 end
