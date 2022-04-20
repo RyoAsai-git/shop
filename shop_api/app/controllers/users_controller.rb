a9779941e0dbc81e9d83f0852d202fd2ae8d7905
Ryo Asai <fu.orangina0516@iCloud.com>
2022-04-20 13:12:42 +0900

    fix:画像更新処理 レスポンスの内容修正

--- shop_api/app/controllers/users_controller.rb
+++ shop_api/app/controllers/users_controller.rb
@@ -18,16 +18,20 @@ class UsersController < ApplicationController
 
   def update
     user = User.find(params[:id])
-    user.avatar.attach(params[:avatar])
+    if user.avatar.attach(params[:avatar])
+      render json: { user_id: user.id, message: '画像のアップロードに成功しました' }, status: 200
+    else
+      render json: { message: '画像をアップロードできませんでした', errors: user.errors.messages }, status: 400
+    end
   end
 
-  def decode(str)
-    Base64.decode64(str.split(',').last)
-  end
+  # def decode(str)
+  #   Base64.decode64(str.split(',').last)
+  # end
 
-  def encode_base64(image_file)
-    image = Base64.encode64(image_file.download) # 画像ファイルをActive Storageでダウンロードし、エンコードする
-    blob = ActiveStorage::Blob.find(image_file[:id]) # Blobを作成
-    "data:#{blob[:content_type]};base64,#{image}" # Vue側でそのまま画像として読み込み出来るBase64文字列にして返す
-  end
+  # def encode_base64(image_file)
+  #   image = Base64.encode64(image_file.download) # 画像ファイルをActive Storageでダウンロードし、エンコードする
+  #   blob = ActiveStorage::Blob.find(image_file[:id]) # Blobを作成
+  #   "data:#{blob[:content_type]};base64,#{image}" # Vue側でそのまま画像として読み込み出来るBase64文字列にして返す
+  # end
 end
