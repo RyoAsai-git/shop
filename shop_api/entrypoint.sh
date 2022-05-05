457d9045d2f9f8bfbe7f99a732e6627c0d4a5c31
Ryo Asai <fu.orangina0516@iCloud.com>
2022-05-06 00:28:06 +0900

    entrypointファイルの不要箇所削除

--- shop_api/entrypoint.sh
+++ shop_api/entrypoint.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-# sudo service nginx start
-# cd /app
-# bin/setup
-# bundle exec pumactl start
+sudo service nginx start
+cd /app
+bin/setup
 
-# Remove a potentially pre-existing server.pid for Rails.
-rm -f /app/tmp/pids/server.pid
-# WARNING:createとseedはfargateの初回のみ実行
-# WARNING:タスクを個々に作って実行の方がいいかも
 bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:seed
 
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
-exec "$@"
+bundle exec pumactl start
+
+# # Remove a potentially pre-existing server.pid for Rails.
+# rm -f /app/tmp/pids/server.pid
+# # WARNING:createとseedはfargateの初回のみ実行
+# # WARNING:タスクを個々に作って実行の方がいいかも
+# bundle exec rails db:create
+# bundle exec rails db:migrate
+# bundle exec rails db:seed
+
+# # Then exec the container's main process (what's set as CMD in the Dockerfile).
+# exec "$@"
