1b4022c6f2aa0e5fc786c4cdd48460bb5fcc3e8f
Ryo Asai <fu.orangina0516@iCloud.com>
2022-05-08 19:55:40 +0900

    fix:entrypoint内のデータベース操作系箇所修正

--- shop_api/entrypoint.sh
+++ shop_api/entrypoint.sh
@@ -4,7 +4,8 @@ sudo service nginx start
 cd /app
 bin/setup
 
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:reset
+# データベース作成用スクリプト
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:reset
 # bundle exec rails db:create
 # bundle exec rails db:migrate
 # bundle exec rails db:seed
