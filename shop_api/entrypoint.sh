a0ac639fb95cb20f7c20d8fa3a49b89134c1b45a
Ryo Asai <fu.orangina0516@iCloud.com>
2022-05-08 19:54:06 +0900

    fix:不要なコメントアウト削除

--- shop_api/entrypoint.sh
+++ shop_api/entrypoint.sh
@@ -10,14 +10,3 @@ DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:reset
 # bundle exec rails db:seed
 
 bundle exec pumactl start
-
-# # Remove a potentially pre-existing server.pid for Rails.
-# rm -f /app/tmp/pids/server.pid
-# # WARNING:createとseedはfargateの初回のみ実行
-# # WARNING:タスクを個々に作って実行の方がいいかも
-# bundle exec rails db:create
-# bundle exec rails db:migrate
-# bundle exec rails db:seed
-
-# # Then exec the container's main process (what's set as CMD in the Dockerfile).
-# exec "$@"
