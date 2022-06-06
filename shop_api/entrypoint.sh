08cad93b2469965e80144303cc6760a46dbcad08
Ryo Asai <fu.orangina0516@iCloud.com>
2022-06-07 00:48:32 +0900

    fix:entrypoint.shファイル db操作箇所修正

--- shop_api/entrypoint.sh
+++ shop_api/entrypoint.sh
@@ -2,12 +2,12 @@
 
 sudo service nginx start
 cd /app
-bin/setup
+# bin/setup
 
 # データベース作成用スクリプト
-# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:reset
-# bundle exec rails db:create
-# bundle exec rails db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:reset
+bundle exec rails db:create
+bundle exec rails db:migrate
 # bundle exec rails db:seed
 
 bundle exec pumactl start
